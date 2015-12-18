4dbedcdd5242e323d2f2bcf125647321093fbec9
Melvin Sowah <mxtcapps@gmail.com>
2015-12-18 20:20:22 +0000

    Implemented board items

--- reduction/component/board.py
+++ reduction/component/board.py
@@ -1,10 +1,12 @@
+from kivy.event import EventDispatcher
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
 
 from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.uix.widget import Widget
 
 
-class Board(FloatLayout):
+class Board(RelativeLayout):
     cell_size = NumericProperty(70)
     columns = NumericProperty(10)
     rows = NumericProperty(5)
@@ -13,3 +15,27 @@ class Board(FloatLayout):
     def do_layout(self, *largs, **kwargs):
         self.size_hint = (None, None)
         self.size = (self.cell_size * self.columns, self.cell_size * self.rows)
+
+        for child in self.children:
+            if not hasattr(child, 'board_pos'):
+                raise Exception('No board_size attribute')
+
+            col, row = child.board_pos
+            child.pos = (
+                self.cell_size * col,
+                self.cell_size * row
+            )
+
+            child.size_hint = (1. / self.columns, 1. / self.rows)
+
+        super(Board, self).do_layout(*largs, **kwargs)
+
+
+class BoardPiece(EventDispatcher):
+    column = NumericProperty(1)
+    row = NumericProperty(0)
+    board_pos = ReferenceListProperty(column, row)
+
+
+class Tile(Widget, BoardPiece):
+    pass
